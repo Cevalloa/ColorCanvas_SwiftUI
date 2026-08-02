ed4c18a364772d810a0dcea44bcbd835b391672a
Alex <Cevalloa@gmail.com>
2026-08-01 23:59:38 -0400

    Initial Commit

---
+++ ColorCanvas_SwiftUI/ColorCanvas_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCanvas_SwiftUIApp.swift
+//  ColorCanvas_SwiftUI
+//
+//  Created by Alex Cevallos on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ColorCanvas_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorCanvas_SwiftUITests/ColorCanvas_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCanvas_SwiftUITests.swift
+//  ColorCanvas_SwiftUITests
+//
+//  Created by Alex Cevallos on 8/1/26.
+//
+
+import Testing
+@testable import ColorCanvas_SwiftUI
+
+struct ColorCanvas_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
