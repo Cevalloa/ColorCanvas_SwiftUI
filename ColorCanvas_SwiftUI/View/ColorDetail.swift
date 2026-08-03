cbb0c9f0fab3a8178d9c7f8d224b6e377c4affc9
Alex <Cevalloa@gmail.com>
2026-08-02 22:01:36 -0400

    Setting up detail view to host color changer

--- ColorCanvas_SwiftUI/View/ColorDetail.swift
+++ ColorCanvas_SwiftUI/View/ColorDetail.swift
@@ -12,8 +12,10 @@ struct ColorDetail: View {
     let colorsDetailViewModel: ColorsDetailViewModel
     @Binding var colorsListViewModel: ColorsListViewModel
     
+    @State private var isShowingSheet = false
+    @State private var selectedColor: Color = .blue
+    
     var body: some View {
-        
         VStack {
             Text("Welcome to the color detail view!")
             HStack {
@@ -23,11 +25,32 @@ struct ColorDetail: View {
             }
             Text("This is one color out of \(colorsListViewModel.colorsInUse.count)")
             HStack {
+                Button("Change this color") {
+                    self.selectedColor = colorsDetailViewModel.color
+                    isShowingSheet = true
+                }.sheet(isPresented: $isShowingSheet) {
+                    addColorView()
+                        .presentationDetents([.medium])
+                }
                 Button("Delete all colors!", role: .destructive) {
                     colorsListViewModel.clearAllColors()
                 }
             }
         }
+    }
+    
+    func addColorView() -> some View {
+        NavigationStack {
+            VStack {
+                ColorPicker("Add a color", selection: $selectedColor, supportsOpacity: false).padding()
+                Circle().foregroundStyle(selectedColor)
+            }
+        }.toolbar {
+            Button("Confirm") {
+                
+            }
+        }
+
     }
 }
 
